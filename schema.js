b02db830961fa169c0e49a172a5df52135dd08d5
Alessandro Holanda <alessandro.holanda@hotmail.com>
2017-09-08 12:05:36 -0300

    fix parse backward compatible to draft-04 also msg.schema as string or object

--- schema.js
+++ schema.js
@@ -6,18 +6,18 @@ module.exports = function(RED) {
         this.name = n.name;
         var node = this;
 
-        console.log(this);
-
         var Ajv = require('ajv');
         var ajv = Ajv({
             allErrors: true,
-            messages: false
+            messages: true
         });
+        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
         
         node.on('input', function(msg) {
             if (msg.payload !== undefined) {
                 console.log(node.func);
-                var validate = ajv.compile(JSON.parse(node.func || msg.schema));
+                var schema = typeof node.func === 'string' && node.func.trim().length ? JSON.parse(node.func) : typeof msg.schema === 'string' ? JSON.parse(msg.schema) : msg.schema;
+                var validate = ajv.compile(schema);
                 var valid = validate(msg.payload);
                 if (!valid) {
                     msg['error'] = validate.errors;
@@ -30,5 +30,5 @@ module.exports = function(RED) {
         });
 
     }
-    RED.nodes.registerType("json-schema", JsonSchemaValidator);
+    RED.nodes.registerType("json-schema-validator", JsonSchemaValidator);
 };
